f3f18cdf9a61676607fd35666183900e283dd4fd
Saad <saadmuizz@gmail.com>
2024-09-16 01:12:47 -0400

    update

--- playwright.config.js
+++ playwright.config.js
@@ -80,9 +80,9 @@ export default defineConfig({
     ],
 
     /* Run your local dev server before starting the tests */
-    webServer: {
-      command: 'npm run preview',
-      url: 'http://127.0.0.1:4173',
-      reuseExistingServer: !process.env.CI,
-    },
+    // webServer: {
+    //   command: 'npm run preview',
+    //   url: 'http://127.0.0.1:4173',
+    //   reuseExistingServer: !process.env.CI,
+    // },
 });
