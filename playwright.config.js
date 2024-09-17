4715f99f7fa912a429baa4eae0d26b7e846218b3
Saad <sshams@rccl.com>
2024-09-17 18:14:38 -0400

    update

--- playwright.config.js
+++ playwright.config.js
@@ -27,7 +27,7 @@ export default defineConfig({
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
+    workers: 1,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [
         ['html', { outputFolder: './test-results' }],
@@ -51,10 +51,10 @@ export default defineConfig({
             name: 'firefox',
             use: { ...devices['Desktop Firefox'] },
         },
-        // {
-        //     name: 'webkit',
-        //     use: { ...devices['Desktop Safari'] },
-        // },
+        {
+            name: 'webkit',
+            use: { ...devices['Desktop Safari'] },
+        },
 
         /* Test against mobile viewports. */
         // {
